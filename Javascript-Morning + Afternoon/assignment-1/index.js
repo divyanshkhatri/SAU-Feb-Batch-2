7a5ac53c60a1d82862f56b7b25eafa2153856cb8
Divyansh Khatri <divyanshkhatri@Divyanshs-MacBook-Pro.local>
2021-02-18 00:08:59 +0530

    Added console logs

--- Javascript-Morning + Afternoon/assignment-1/index.js
+++ Javascript-Morning + Afternoon/assignment-1/index.js
@@ -1,14 +1,18 @@
 length = () => {
     document.getElementById("stringlength").innerHTML = "Length of the string is " + document.getElementById("string").value.length + ".";
+    console.log(document.getElementById("string").value.length);
 }
 
 lowerupper = () => {
     document.getElementById("stringlowerupper").innerHTML =  "Lower Case: " + document.getElementById("string").value.toLowerCase() + 
     ", " + "Upper Case: " + document.getElementById("string").value.toUpperCase();
+    console.log(document.getElementById("string").value.toUpperCase());
+    console.log(document.getElementById("string").value.toLowerCase());
 }
 
 vowelsconsonants = () => {
     let s = document.getElementById("string").value;
+    console.log(s);
     let strVowels = "";
     let strConsonants = "";
     let strOtherChars = "";
@@ -30,6 +34,9 @@ vowelsconsonants = () => {
             strConsonants += s.charAt(i) + ", ";
         }
     }
+    console.log("Vowels are: " + strVowels);
+    console.log("Consonants are: "+ strConsonants);
+    console.log("Other characters are: " + strOtherChars);
 
     document.getElementById("stringvowelsconsonants").innerHTML = "Vowels are - " + strVowels + ". Consonants are " + strConsonants + ". Other characters are " + strOtherChars;
     
@@ -38,12 +45,16 @@ vowelsconsonants = () => {
 isnumber = () => {
 
     let s= document.getElementById("string").value;
+    console.log(s);
     let i;
     for(i = 0; i<s.length; i++) {
         if(s.charAt(i) >=0 || s.charAt(i) <=9) {
             document.getElementById("stringisnumber").innerHTML = "The string has atleast one number."
+            console.log("The string has atleast one number.");
             return;
         }
     }
     document.getElementById("stringisnumber").innerHTML = "The string has no numbers."
+    console.log("The string has no numbers.");
+
 }
